e9ad25bb3a50cacb886f4bf3cef462a82a9e0eb9
TamakiAkoo <Tamakiakoo@163.com>
2021-05-13 21:38:48 +0800

    函数式组件

--- geektime-vue-1/演示DEMO源码/src/views/1.6/TempVar.js
+++ geektime-vue-1/演示DEMO源码/src/views/1.6/TempVar.js
@@ -1,6 +1,8 @@
 export default {
   functional: true,
   render: (h, ctx) => {
+    //作用域插槽的本质是方法，而这个方法就是scopedSlots.xxx 
+    //这个方法接收的参数就是传递的变量 其实等价于 <slot v-bind="props"/>
     return ctx.scopedSlots.default && ctx.scopedSlots.default(ctx.props || {});
   }
 };
