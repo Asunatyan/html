321d72e5d68c7f65314c191e6453127bfae4634f
dingqiaokun <12345678>
2021-05-14 16:54:22 +0800

    指令的本质

--- geektime-vue-1/演示DEMO源码/src/views/1.7/Directives.vue
+++ geektime-vue-1/演示DEMO源码/src/views/1.7/Directives.vue
@@ -1,29 +1,38 @@
 <template>
   <div>
     <h2>v-text</h2>
-    <div v-text="'hello vue'">hello world</div>
+    <div v-text="'hello vue 会覆盖里面的值'">hello world</div>
+    <hr>
     <h2>v-html</h2>
-    <div v-html="'<span style=&quot;color: red&quot;>hello vue</span>'">
-      hello world
+    <div v-html="'<span style=&quot;color: red&quot;>hello vue--里面的的值显示不粗来了-</span>'">
+      hello world v-html
     </div>
+    <hr>
     <h2>v-show</h2>
     <div v-show="show">hello vue</div>
     <button @click="show = !show">change show</button>
+    <hr>
     <h2>v-if v-esle-if v-else</h2>
     <div v-if="number === 1">hello vue {{ number }}</div>
     <div v-else-if="number === 2">hello world {{ number }}</div>
     <div v-else>hello geektime {{ number }}</div>
+    <hr>
     <h2>v-for v-bind</h2>
     <div v-for="num in [1, 2, 3]" v-bind:key="num">hello vue {{ num }}</div>
+    <hr>
     <h2>v-on</h2>
     <button v-on:click="number = number + 1">number++</button>
+    <hr>
     <h2>v-model</h2>
+    {{ message }}
     <input v-model="message" />
+    <hr>
     <h2>v-pre</h2>
     <div v-pre>{{ this will not be compiled }}</div>
+    <hr>
     <h2>v-once</h2>
     <div v-once>
-      {{ number }}
+      {{ "只执行一次" +number }}
     </div>
   </div>
 </template>
