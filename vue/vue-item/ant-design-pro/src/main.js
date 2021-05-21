13ed85498166fd32ced13017239f89e7144279fa
dingqiaokun <12345678>
2021-05-21 18:37:07 +0800

    如何自定义Webpack和Babel配置

--- vue/vue-item/ant-design-pro/src/main.js
+++ vue/vue-item/ant-design-pro/src/main.js
@@ -3,10 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-Vue.config.productionTip = false;
+//import Antd from "ant-design-vue";
+//import 'ant-design-vue/dist/antd.css';//引入样式
+//import 'ant-design-vue/dist/antd.less';//引入样式全部
 
+import Button from "ant-design-vue/lib/button";
+import 'ant-design-vue/lib/button/style';//引入样式部分
+
+
+Vue.config.productionTip = false;
+Vue.use(Button)
 new Vue({
   router,
   store,
+  Button,
   render: (h) => h(App),
 }).$mount("#app");
