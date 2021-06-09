258f8b7e8f4093f2ec795bb5f8f1b5180f566cc9
tamakiako <tamakiako@164.com>
2021-06-09 23:22:58 +0800

    add axios

--- vue/miniapp/src/views/mm/Miaomiao.vue
+++ vue/miniapp/src/views/mm/Miaomiao.vue
@@ -1,12 +1,27 @@
 <template>
   <a-table :columns="columns" :data-source="data">
-    <a slot="name" slot-scope="text">{{ text }}</a>
+    <template v-slot:name="slotProps">
+      <a>{{ slotProps }}11111111111</a>
+    </template>
+    <!-- 
+       下面的写法已经废弃了使用上面的写法  来获取子组件中才有的数据 
+
+       子组件的定义类似小面这样的
+       <span>
+        <slot name="name" v-bind:data="data">
+          {{ data.name }}
+        </slot>
+      </span>
+       -->
+    <!-- <a slot="name" slot-scope="text">{{ text }}</a> -->
     <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
     <span slot="tags" slot-scope="tags">
       <a-tag
         v-for="tag in tags"
         :key="tag"
-        :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+        :color="
+          tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'
+        "
       >
         {{ tag.toUpperCase() }}
       </a-tag>
@@ -23,64 +38,78 @@
 <script>
 const columns = [
   {
-    dataIndex: 'name',
-    key: 'name',
-    slots: { title: 'customTitle' },
-    scopedSlots: { customRender: 'name' },
+    dataIndex: "name",
+    key: "name",
+    slots: { title: "customTitle" },
+    scopedSlots: { customRender: "name" },
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
   },
   {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    scopedSlots: { customRender: 'tags' },
+    title: "Tags",
+    key: "tags",
+    dataIndex: "tags",
+    scopedSlots: { customRender: "tags" },
   },
   {
-    title: 'Action',
-    key: 'action',
-    scopedSlots: { customRender: 'action' },
+    title: "Action",
+    key: "action",
+    scopedSlots: { customRender: "action" },
   },
 ];
 
 const data = [
   {
-    key: '1',
-    name: 'John Brown',
+    key: "1",
+    name: "John Brown",
     age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
   },
   {
-    key: '2',
-    name: 'Jim Green',
+    key: "2",
+    name: "Jim Green",
     age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
   },
   {
-    key: '3',
-    name: 'Joe Black',
+    key: "3",
+    name: "Joe Black",
     age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
   },
 ];
-
+import { getHomeMultidata } from "network/home"
 export default {
   data() {
     return {
       data,
       columns,
     };
+  },
+  methods:{
+    getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.data = res;
+          console.log(this.data);
+        })
+      }
+  },
+  created() {
+    // 1.请求多个数据
+    this.getHomeMultidata();
+
+    // 2.请求商品数据
   },
 };
 </script>
